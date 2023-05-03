01f621b700e0e0956977bc899648dde60db150c7
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-05-02 21:35:49 -0300

    feat: create route to create a product.

---
+++ src/infrastructure/api/routes/product.route.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import ProductRepository from "../../product/repository/sequelize/product.repository";
+import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
+
+export const productRoute = express.Router();
+
+productRoute.post("/", async (req: Request, res: Response) => {
+    const usecase = new CreateProductUseCase(new ProductRepository());
+    try {
+        const productDto = {
+            name: req.body.name,
+            price: req.body.price,
+        };
+        const output = await usecase.execute(productDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
