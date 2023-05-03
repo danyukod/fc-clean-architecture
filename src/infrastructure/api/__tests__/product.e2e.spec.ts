faf47ca9f2388a05f861fedabae543e5a7407bb2
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-05-02 21:37:58 -0300

    feat: create e2e test to list all products.

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -22,4 +22,35 @@ describe("E2E test for product", () => {
         expect(response.body.name).toBe("Product 1");
         expect(response.body.price).toBe(100);
     });
+
+    it("should not create a product", async () => {
+        const response = await request(app).post("/product").send({
+            name: "Product 1",
+        });
+        expect(response.status).toBe(500);
+    });
+
+    it("should list all product", async () => {
+        const response = await request(app)
+            .post("/product")
+            .send({
+                name: "Product 1",
+                price: 100,
+            });
+        expect(response.status).toBe(200);
+        const response2 = await request(app)
+            .post("/product")
+            .send({
+                name: "Product 2",
+                price: 200,
+            });
+        expect(response2.status).toBe(200);
+        const response3 = await request(app).get("/product");
+        expect(response3.status).toBe(200);
+        expect(response3.body.length).toBe(2);
+        expect(response3.body[0].name).toBe("Product 1");
+        expect(response3.body[0].price).toBe(100);
+        expect(response3.body[1].name).toBe("Product 2");
+        expect(response3.body[1].price).toBe(200);
+    });
 });
